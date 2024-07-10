a6b4633ef5a25d03779e321a16b12734b51824f8
Josiane Souza <josiane.rosa.souza@gmail.com>
2024-07-09 23:21:56 -0300

    First commit

---
+++ JavaWithSpringBoot/rest-with-spring-boot/src/test/java/br/com/josierosa/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.josierosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
